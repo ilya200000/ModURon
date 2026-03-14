7eaa99271b5b9227a8e09fb1fe0d0af4e96067cb
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-03-14 11:07:14 +0500

    Update FeatherFinch.java

--- src/main/java/feather/finch/FeatherFinch.java
+++ src/main/java/feather/finch/FeatherFinch.java
@@ -1,24 +1,16 @@
 package feather.finch;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FeatherFinch implements ModInitializer {
-	public static final String MOD_ID = "featherfinch";
+    public static final String MOD_ID = "featherfinch";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+        // Тут пока пусто, чтобы ничего не ломалось
+        LOGGER.info("FeatherFinch загружен. Жду приказов!");
+    }
 }
