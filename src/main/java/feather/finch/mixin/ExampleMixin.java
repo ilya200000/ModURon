1b98614d514695586904b76bf2f9bc393a9b60b9
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-03-14 11:08:12 +0500

    Update ExampleMixin.java

--- src/main/java/feather/finch/mixin/ExampleMixin.java
+++ src/main/java/feather/finch/mixin/ExampleMixin.java
@@ -1,15 +1,28 @@
 package feather.finch.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftServer.class)
+@Mixin(LivingEntity.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+    
+    @Inject(method = "damage", at = @At("RETURN"))
+    private void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        // Проверяем, что урон прошел (cir.getReturnValue() == true) и атаковал игрок
+        if (cir.getReturnValue() && source.getAttacker() != null && source.getAttacker().isPlayer()) {
+            
+            // Формируем сообщение: "Урон: [число]" красным цветом
+            Text message = Text.literal("Урон: ")
+                .append(Text.literal(String.format("%.1f", amount)).formatted(Formatting.RED));
+
+            // Отправляем атакующему в Actionbar (над хотбаром)
+            source.getAttacker().sendMessage(message, true);
+        }
+    }
 }
